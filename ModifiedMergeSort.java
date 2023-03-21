73ff0ac8444ea0ca77dfa951b5388bd0d28f60a5
Janriz Libres <leonmadrik11037@gmail.com>
2023-03-21 23:22:20 +0800

    cleaned up code

--- ModifiedMergeSort.java
+++ ModifiedMergeSort.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 public class ModifiedMergeSort {
 
-    // Function to count the number of inversions
-    // during the merge process
+    // Function to count the number of inversions during the merge process
     private static int mergeAndCount(int[] arr, int l, int m, int r) {
 
         // Left subarray
@@ -15,12 +14,13 @@ public class ModifiedMergeSort {
         int i = 0, j = 0, k = l, swaps = 0;
 
         while (i < left.length && j < right.length) {
-            if (left[i] <= right[j])
+            if (left[i] <= right[j]) {
                 arr[k++] = left[i++];
-            else {
-                arr[k++] = right[j++];
-                swaps += (m + 1) - (l + i);
+                continue;
             }
+
+            arr[k++] = right[j++];
+            swaps += (m + 1) - (l + i);
         }
 
         while (i < left.length)
@@ -35,69 +35,51 @@ public class ModifiedMergeSort {
     // Merge sort function
     private static int mergeSortAndCount(int[] arr, int l, int r) {
 
-        // Keeps track of the inversion count at a
-        // particular node of the recursion tree
-        int count = 0;
+        // Keeps track of the inversion count at a particular node of the recursion tree
+        int cnt = 0;
 
         if (l < r) {
             int m = (l + r) / 2;
 
-            // Total inversion count = left subarray count
-            // + right subarray count + merge count
+            // Total inversion count = left subarray count + right subarray count + merge count
 
             // Left subarray count
-            count += mergeSortAndCount(arr, l, m);
+            cnt += mergeSortAndCount(arr, l, m);
 
             // Right subarray count
-            count += mergeSortAndCount(arr, m + 1, r);
+            cnt += mergeSortAndCount(arr, m + 1, r);
 
             // Merge count
-            count += mergeAndCount(arr, l, m, r);
+            cnt += mergeAndCount(arr, l, m, r);
         }
 
-        return count;
+        return cnt;
     }
 
-    private static boolean isSolvable(int[] arr) {
-        int cnt = mergeSortAndCount(arr, 0, arr.length - 1);
+    private static boolean isSolvable(int[] conf) {
+        int cnt = mergeSortAndCount(conf, 0, conf.length - 1);
         System.out.println("\nInversion count: " + cnt);
         return cnt % 2 == 0;
     }
 
     public static void main(String[] args) {
-        String[] configs = {
-            "12364-785", // 12346-785   1 diff
-                         // 123456-78   3 diffs
-                         // 1234567-8   
-                         // 12345678-   
-            "12368457-", // 12346857-   2 diffs
-                         // 12345687-   2 diffs
-                         // 12345678-   1 diff
-            "15-328467", // 125-38467   2 diffs
-                         // 1235-8467   1 diff
-                         // 12345-867   2 diffs
-                         // 123456-87   1 diff
-                         // 1234567-8   1 diffs
-                         // 12345678- 
-            "57814623-",
-            "357248-61"
-        };
+        String[] configs = { "12364-785", "12368457-", "15-328467", "57814623-", "357248-61" };
 
         for (String config : configs) {
             String[] chars = config.split("");
-            int[] equiv = new int[config.length() - 1];
+            int[] conf = new int[config.length() - 1];
 
             int idx = 0;
             for (int i = 0; i < chars.length; i++) {
                 try {
-                    equiv[idx] = Integer.parseInt(chars[i]);
+                    conf[idx] = Integer.parseInt(chars[i]);
                     idx++;
                 } catch (NumberFormatException nfe) {
                     continue;
                 }
             }
 
-            System.out.println(isSolvable(equiv) ? "Solvable." : "Not Solvable.");
+            System.out.println(isSolvable(conf) ? "Solvable." : "Not Solvable.");
         }
     }
 }
